eb45af1999518cd4da574ebd8207227047561f11
Sneheel <shivamsneheel@gmail.com>
2023-04-04 10:44:44 +0530

    Initial commit: project setup with basic structure and dependencies added.

---
+++ src/main/java/com/bourne/notepass/NotepassApplication.java
@@ -0,0 +1,13 @@
+package com.bourne.notepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepassApplication.class, args);
+	}
+
+}
